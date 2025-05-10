096b30ba5f18eb9212d142df05544f8cab4cfd60
Guto Xavier <gutoffline@gmail.com>
2025-05-09 21:18:55 -0300

    funções

--- app01.js
+++ app01.js
@@ -133,6 +133,15 @@ frutas.push("🍕")
 
 
 // Funções básicas
+function soma(num1, num2){
+    let total = num1 + num2
+    console.log("O resultado é " + total)
+}
+
+soma(100 , 14)
+soma(7 , 3)
+
+
 // Escopo
 
 
